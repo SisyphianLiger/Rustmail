eeb6995d2c02730691e4d9450a4e788c514da939
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-07-10 14:33:21 +0200

    Hello World Actix Created

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
-use::zero2prod::run;
-use std::net::TcpListener;
-/*
-        HttpServer --> Handles incoming requests, and is the transport layer
-        Application --> Routing/Middleware/requests exists
-        Endpoint(.route) --> creates the webpath end 
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
- */
-
-
-
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     let name = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello {}!", &name)
-// }
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to random port");
-
-    run(listener)?.await
+    println!("Go to 127.0.0.1:7878");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:7878")?
+    .run()
+    .await
 }
-
