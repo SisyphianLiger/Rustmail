3489fa56d1c9514f735f19e9f9b09253c2572813
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-07-11 20:50:05 +0200

    Revert "Woopsie Daisie on Cargo.toml"
    This reverts commit 0a916fc4299cb51db985b7ed671edf8e518af21f.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use zero2prod::run;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run().await
+    println!("Go to 127.0.0.1:7878");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:7878")?
+    .run()
+    .await
 }
