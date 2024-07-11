0a916fc4299cb51db985b7ed671edf8e518af21f
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-07-11 07:38:41 +0200

    Refactoring from main to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,22 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
 
+pub async fn run() -> Result<(), std::io::Error> {
+    println!("Go to 127.0.0.1:7878");
+
+    // HttpServer handles the Transport Level
+    HttpServer::new(|| {
+        // Established a new connection with a client
+        App::new()
+            // The routes of requests we see here
+            // Can also handle middleware etc
+            .route("/health_check", web::get().to(health_check))
+    })
+    // The port we will bind to:w
+    .bind("127.0.0.1:7878")?
+    .run()
+    .await
+}
