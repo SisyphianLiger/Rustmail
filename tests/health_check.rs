6cb1718b351b901b905509c09385c7ac9fff35c9
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-07-12 12:23:02 +0200

    Tests for subscriptions passed

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -37,3 +37,57 @@ async fn spawn_app() -> String {
     // returning String for the caller
     format!("{}{}", http, port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Creating the app
+    let app_address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscribe", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to Execute Request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_with_incorrect_data_returns_400() {
+    //Creating app
+    let app_address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Make a Response
+        let response = client
+            .post(&format!("{}/subscribe", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to Execute Request.");
+
+        // Assert to check that the status is 400 i.e. failed
+        // But with a vector we can add a message to give specific info for
+        // the failure
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customized information on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
